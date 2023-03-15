5a36756e8a6ca58f40d17dd93e2c0bd00edeb116
Syota Nishimura <syota4723@gmail.com>
2023-03-15 11:29:10 +0900

    :card_file_box: add: Models and Migrations

--- database/migrations/2023_03_15_015539_create_category_posts_table.php
+++ database/migrations/2023_03_15_015539_create_category_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('category_post', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
