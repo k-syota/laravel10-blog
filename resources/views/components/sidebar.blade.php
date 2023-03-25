41b4fe5311843866ce2f45f941807f93b9a691c2
Syota Nishimura <syota4723@gmail.com>
2023-03-25 17:30:25 +0900

    :bug: Minor Bug fixes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -6,7 +6,7 @@
             @foreach ($categories as $category)
                 <a href="{{ route('by-category', $category) }}"
                     class="font-semibold block py-2 px-3 rounded
-                    {{ request('category')->slug == $category->slug ? 'bg-blue-600 text-white' : '' }}">
+                    {{ request('category')?->slug == $category->slug ? 'bg-blue-600 text-white' : '' }}">
                     {{ $category->title }}({{ $category->total }})
                 </a>
             @endforeach
